7c75ad16c325661b14a7a62693e8274ae8fd30c8
dcirlan <kirlan@hotmail.fr>
2020-11-30 12:30:54 +0100

    réalisation challenge

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,18 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
+         .copyFiles({
+                 from: './assets/images',
+        
+                 // optional target path, relative to the output dir
+                 //to: 'images/[path][name].[ext]',
+        
+                 // if versioning is enabled, add the file hash too
+                 //to: 'images/[path][name].[hash:8].[ext]',
+        
+                 // only copy files matching this pattern
+                 //pattern: /\.(png|jpg|jpeg)$/
+             })
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
@@ -54,7 +66,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+.enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -64,7 +76,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+.autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
