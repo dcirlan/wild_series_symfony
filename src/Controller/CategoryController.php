440a8228b507822e451afab078c896543b2c91f3
dcirlan <kirlan@hotmail.fr>
2021-01-11 11:01:02 +0100

    feature : added forms to add categories and programms

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,11 +2,13 @@
 // src/Controller/CategoryController.php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Program;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Category;
-use App\Entity\Program;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/categories", name="category_")
@@ -32,6 +34,44 @@ Class CategoryController extends AbstractController
             ['categories' => $categories]
         );
     }
+
+        /**
+         * The controller for the category add form
+         * @Route("/new", name="new")
+         */
+        public function new(Request $request) : Response
+        {    
+            // Create a new Category Object
+            $category = new Category();
+    
+            // Create the associated Form
+            $form = $this->createForm(CategoryType::class, $category);
+    
+            // Get data from HTTP request
+            $form->handleRequest($request);
+    
+            // Was the form submitted ?
+            if ($form->isSubmitted()) {
+                // Deal with the submitted data
+
+                // Get the Entity Manager
+                $entityManager = $this->getDoctrine()->getManager();
+
+                // Persist Category Object
+                $entityManager->persist($category);
+
+                // Flush the persisted object
+                $entityManager->flush();
+
+                // Finally redirect to categories list
+                return $this->redirectToRoute('category_index');
+            }
+    
+            // Render the form
+            return $this->render('category/new.html.twig', ["form" => $form->createView()]);
+        }    
+    
+        // L'action show() se trouvera plus bas
 
     /**
      * @Route("/{categoryName}", name = "show")
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,13 +2,15 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Program;
 use App\Entity\Season;
 use App\Entity\Episode;
+use App\Entity\Program;
+use App\Form\ProgramType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Route("/programs", name="program_")
@@ -34,6 +36,43 @@ Class ProgramController extends AbstractController
             ['programs' => $programs]
         );
     }
+
+            /**
+         * The controller for the category add form
+         * @Route("/new", name="new")
+         */
+        public function new(Request $request) : Response
+        {    
+            // Create a new Program Object
+            $program = new Program();
+    
+            // Create the associated Form
+            $form = $this->createForm(ProgramType::class, $program);
+    
+            // Get data from HTTP request
+            $form->handleRequest($request);
+    
+            // Was the form submitted ?
+            if ($form->isSubmitted()) {
+                // Deal with the submitted data
+
+                // Get the Entity Manager
+                $entityManager = $this->getDoctrine()->getManager();
+
+                // Persist Category Object
+                $entityManager->persist($program);
+
+                // Flush the persisted object
+                $entityManager->flush();
+
+                // Finally redirect to categories list
+                return $this->redirectToRoute('program_index');
+            }
+    
+            // Render the form
+            return $this->render('program/new.html.twig', ["form" => $form->createView()]);
+        }    
+
 
     /**
      * Getting a program by id
